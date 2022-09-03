fbf015b156f09e5295e2e78a79cb915a89e179db
pahry <pahridilalintang.ti@gmail.com>
2022-09-03 16:12:54 +0700

    Revert "blading template laravel"
    This reverts commit 90ba02344a20ce78b068613f397c82922f5568c5.

--- readme.txt
+++ readme.txt
@@ -5,8 +5,12 @@ Membuat Website Instansi Menggunakan laravel Modular
 4. dashboard admin terdiri dari beberapa Modul seperti :
     a. Manajamen User
     b. Manajemen Blog
+<<<<<<< HEAD
 
 Penjelasan Fitur
 User Controller Berisi semua halaman yang dapat diakses oleh user / pengunjung website
 
 Versi Laravel yang digunakan 9.27.0
+=======
+test after revert
+>>>>>>> parent of 90ba023 (blading template laravel)
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,7 +1,34 @@
-@extends('user/master')
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Module ManajUser</title>
 
-@section('konten')
+       {{-- Laravel Mix - CSS File --}}
+       <!-- {{-- <link rel="stylesheet" href="{{ mix('css/manajuser.css') }}"> --}} -->
 
-	<h1>Ini Adalah Halaman Home</h1>
+    </head>
+    <body>
+        <!-- @yield('content') -->
+        <h1>Ini Halaman User</h1>
 
-@endsection
+
+        <ul class="menu-content">
+            <li><a href="" class="menu-item">Dashboard</a></li>
+            <li><a href="/user" class="menu-item">User</a></li>
+            <li><a href="/about" class="menu-item">About</a></li>
+        </ul>
+
+        <!-- <a>Halaman Manaj User</a> -->
+
+
+
+
+
+        {{-- Laravel Mix - JS File --}}
+        <!-- {{-- <script src="{{ mix('js/manajuser.js') }}"></script> --}} -->
+    </body>
+</html>
+
